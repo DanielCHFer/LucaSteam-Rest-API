b9ee805d9b58e9c79f5e0d5622ab44804161bfb0
Dani <danielchfer@gmail.com>
2024-11-29 10:08:02 +0100

    Commit Inicial Carcasa

---
+++ src/main/java/com/ejemplos/spring/RestLucaSteamApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplos.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLucaSteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLucaSteamApplication.class, args);
+	}
+
+}
