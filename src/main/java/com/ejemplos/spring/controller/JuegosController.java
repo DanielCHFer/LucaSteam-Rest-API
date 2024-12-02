0ab5ca7fbe8b437b089805aaf1e0ef41a0b1e9b6
PatriciaGS00 <patri.adri.98@gmail.com>
2024-12-02 09:34:16 +0100

    endpoint read

--- src/main/java/com/ejemplos/spring/controller/JuegosController.java
+++ src/main/java/com/ejemplos/spring/controller/JuegosController.java
@@ -4,20 +4,49 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ejemplos.spring.model.Juego;
 import com.ejemplos.spring.service.JuegosService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@RequestMapping("/juegos")
+@Tag(name = "Juegos Controller", description = "Controlador para gestionar los juegos.")
 public class JuegosController {
 	
 	@Autowired
 	private JuegosService serv;
 	
-	@GetMapping("/juegos")
+	/**
+	 * Listar todos los juegos
+	 *
+	 * @return La lista de objetos Juego
+	 */
+	@Operation(
+		summary = "Listar Juegos",
+		description = "Devuelve una lista de todos los juegos almacenados en la base de datos."
+	)
+	@GetMapping
 	public List<Juego> listJuegos(){
 		return serv.findAll();
 	}
+	
+	/**
+	 * Cargar juegos desde un archivo CSV.
+	 *
+	 * @return Una lista de juegos con los datos del csv para agregarlos a la base de datos.
+	 */
+	@Operation(
+		summary = "Cargar juegos desde CSV",
+		description = "Carga los juegos desde un archivo CSV y devuelve la lista de juegos."
+	)
+	@GetMapping("/cargar")
+    public List<Juego> readJuegos() {
+        return serv.readJuegos();
+    }
 
 }
