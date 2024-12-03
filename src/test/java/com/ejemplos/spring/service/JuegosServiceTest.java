d3534ba219022b4fbca5bbab9125c21bcc770271
Dani <danielchfer@gmail.com>
2024-12-03 11:13:24 +0100

    Test Servicios

--- src/test/java/com/ejemplos/spring/service/JuegosServiceTest.java
+++ src/test/java/com/ejemplos/spring/service/JuegosServiceTest.java
@@ -1,53 +1,17 @@
 package com.ejemplos.spring.service;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-<<<<<<< HEAD
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-=======
->>>>>>> dani
-
-import java.util.Collections;
 
 import com.ejemplos.spring.model.Juego;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.Test;
-<<<<<<< HEAD
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-@WebMvcTest(JuegosService.class)
-class JuegosServiceTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-    
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private JuegosService juegoService; // Simula el servicio inyectado en el controlador
-
-    @Test
-	void testDeleteJuegoDevuelveJuego() throws Exception {
-		//test
-	}
-    
-=======
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.ejemplos.spring.model.Juego;
 import com.ejemplos.spring.repository.JuegosDAO;
 
 @SpringBootTest
@@ -66,5 +30,4 @@ public class JuegosServiceTest {
 		verify(juegoRepository, times(1)).save((any(Juego.class)));
 	}
 
->>>>>>> dani
 }
