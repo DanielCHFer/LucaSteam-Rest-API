7c12aa6190b1a477107729b27551ef17598fe563
Adrian <adrian.gomez@formacion.luca-tic.com>
2024-12-03 13:19:18 +0100

    borro todo a tomar por culo

--- src/main/java/com/ejemplos/spring/controller/error/JuegoFormatException.java
+++
@@ -1,8 +0,0 @@
-package com.ejemplos.spring.controller.error;
-
-public class JuegoFormatException extends RuntimeException{
-
-	 public JuegoFormatException(String message) {
-	        super(message);
-	    }
-}
