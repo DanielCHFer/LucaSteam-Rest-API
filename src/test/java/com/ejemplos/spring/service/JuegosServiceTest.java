8c3e3f4bc9e505ca0eb7eb65bcea9e931cd5c5fe
Dani <danielchfer@gmail.com>
2024-12-03 11:05:29 +0100

    Test Servicios

--- src/test/java/com/ejemplos/spring/service/JuegosServiceTest.java
+++ src/test/java/com/ejemplos/spring/service/JuegosServiceTest.java
@@ -1,36 +1,35 @@
 package com.ejemplos.spring.service;
 
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Optional;
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 import com.ejemplos.spring.model.Juego;
-
-import io.swagger.v3.oas.models.media.MediaType;
+import com.ejemplos.spring.repository.JuegosDAO;
 
 @SpringBootTest
 public class JuegosServiceTest {
-	
-	@Autowired
-    private MockMvc mockMvc;
 
-    @MockBean
-    private JuegosService juegoService;
-	
+	@Mock
+	JuegosDAO juegoRepository;
+
+	@InjectMocks
+	JuegosServiceImpl juegoService;
+
 	@Test
-	void testDeleteJuegoDevuelveJuego() throws Exception {
-		//test
+	void testSeInsertaJuego() {
+		when(juegoRepository.save(any(Juego.class))).thenReturn(new Juego());
+		juegoService.saveJuego(new Juego());
+		verify(juegoRepository, times(1)).save((any(Juego.class)));
 	}
-
 
 }
